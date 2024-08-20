3404615613bbc18bd6e12dd50c0f9257a3b0ad2b
Michele Cheow <michelehcheow@gmail.com>
2024-08-20 11:28:28 -0700

    added constructor, injectRepository injects the Repository Name and assiged to nameRepo prop, changed addName method to add new name to db

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Name } from './name.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Name)
+    private namesRepository: Repository<Name>,
+  ) {}
 
   async addName(name: string) {
-    console.log("Name: ", name);
-    return { name };
+    const nameData = await this.namesRepository.save({ name });
+    return nameData;
   }
 
   async getNames(){
     return {};
   }
 }
+
