dc4f70bf3e9d8958a346da5dd9cdbaadc0b80643
Michele Cheow <michelehcheow@gmail.com>
2024-08-20 11:12:48 -0700

    set logging to true, added forFeature for Name entitity, added parseInt for db port, add config module to imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,19 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Name } from './name.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      port: parseInt(process.env.DB_PORT ?? '0'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [Name],
       synchronize: true,
+      logging: true,
     }),
+    TypeOrmModule.forFeature([Name]),
   ],
   controllers: [AppController],
   providers: [AppService],
