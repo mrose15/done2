efc81f42504fbb43e3633ab909cc494611c04a78
Michele Cheow <michelehcheow@gmail.com>
2024-08-21 15:12:24 -0700

    used TypeOrmModule.forRootAsync method instead of TypeOrmModule.forRoot

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Name } from './name.entity';
+import typeorm from './config/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '0'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Name],
-      synchronize: true, // not for prod
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('typeorm') as TypeOrmModuleOptions),
     }),
     TypeOrmModule.forFeature([Name]),
   ],
