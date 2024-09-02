75a6f557a9b9f3729d6599d40a221bed672fbb27
Michele Cheow <michelehcheow@gmail.com>
2024-09-02 16:52:34 -0700

    log fake token to test

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   signUp(signupDto) {
-    return signupDto;
+    //return signupDto;
+    console.log('Signup:', signupDto);
+    return 'fake token'; // Replace with actual JWT token generation logic
   }
 }
