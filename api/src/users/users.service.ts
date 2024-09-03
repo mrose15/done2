11de56c969a71b3e2fb953a0cbab16f772c8186f
Michele Cheow <michelehcheow@gmail.com>
2024-09-02 19:02:46 -0700

    added users and new create user table migrations

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignupDto } from 'src/auth/auth.controller';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async createUser(user: SignupDto) {
+    console.log('User:', user);
+    return await this.usersRepository.save(user);
+  }
+}
