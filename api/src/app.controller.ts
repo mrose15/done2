a400855311ab67476c5564460fb03f618206856b
Michele Cheow <michelehcheow@gmail.com>
2024-09-02 19:01:48 -0700

    deleted test db, added user table

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -2,16 +2,4 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post('/name')
-  async addName(@Body('firstName') firstName: string, @Body('lastName') lastName: string) {
-    return await this.appService.addName(firstName, lastName);
-  }
-
-  @Get()
-  async getNames() {
-    return await this.appService.getNames();
-  }
-}
+export class AppController {}
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,23 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Name } from './name.entity';
 
 @Injectable()
-export class AppService {
-  constructor(
-    @InjectRepository(Name)
-    private namesRepository: Repository<Name>,
-  ) {}
-
-  async addName(firstName: string, lastName: string) {
-    await this.namesRepository.save({ first_name: firstName, last_name: lastName });
-    return await this.getNames();
-  }
-
-  async getNames(){
-    // get all names from the database
-    return await this.namesRepository.find();
-  }
-}
-
+export class AppService {}
