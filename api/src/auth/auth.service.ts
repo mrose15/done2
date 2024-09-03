755148b4d922ac75a2c999a55e3557bae380e5a6
Michele Cheow <michelehcheow@gmail.com>
2024-09-02 19:05:15 -0700

    added bcrypt, hashed passwords, added user service

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  signUp(signupDto) {
-    //return signupDto;
-    console.log('Signup:', signupDto);
+  constructor(private usersService: UsersService) {}
+
+  async hashPassword(password: string) {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+  }
+
+  async signUp(signupDto) {
+    //hash password
+    const hashedPassword = await this.hashPassword(signupDto.password);
+    signupDto.password = hashedPassword;
+
+    // add user to user table
+    this.usersService.createUser(signupDto);
     return 'fake token'; // Replace with actual JWT token generation logic
   }
 }
