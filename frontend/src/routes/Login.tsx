998439ff8e372a6a3f934e539ff8db8080af2dae
Michele Cheow <michelehcheow@gmail.com>
2024-09-06 10:40:37 -0700

    started Login form

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,37 +1,147 @@
+import {
+  Box,
+  Button,
+  chakra,
+  Flex,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import InputField from "../components/Forms/InputField";
+import PasswordField from "../components/Forms/PasswordField";
+
+import { useForm } from "react-hook-form";
+import { FormData, UserSchema, ValidFieldNames } from "../types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<FormData>({
+    mode: "onTouched",
+    resolver: zodResolver(UserSchema),
+  });
+
+  const submitHandler = async (data: FormData) => {
+    console.log("username", data.username);
+    console.log("password", data.password);
+    console.log("data", data);
+    try {
+      const res = await axios.post("http://localhost:3025/auth/login", data);
+      const { errors = {} } = res.data; // Destructure the 'errors' property from the response data
+
+      const token = res.data;
+      //TODO: store with cookie
+      localStorage.setItem("token", token);
+
+      //Define a mapping between server-side field names and their corresponding client-side names
+      const fieldErrorMapping: Record<string, ValidFieldNames> = {
+        username: "username",
+        password: "password",
+      };
+
+      //Find the first field with an error in the response data
+      const fieldWithError = Object.keys(fieldErrorMapping).find(
+        (field) => errors[field]
+      );
+
+      // If a field with an error is found, update the form error state using setError
+      if (fieldWithError) {
+        setError(fieldErrorMapping[fieldWithError], {
+          type: "server",
+          message: errors[fieldWithError],
+        });
+      }
+
+      navigate("/projects");
+    } catch (error) {
+      const errors = (error as any)?.response.data;
+
+      if (errors.statusCode > 200) {
+        setError("root.serverError", {
+          type: errors.statusCode,
+          message:
+            "There was an error logging in. Please check your username and password and try again.",
+        });
+      }
+
+      if (errors.message.includes("username")) {
+        setError("username", {
+          type: "server",
+          message: "Username is incorrect",
+        });
+      }
+
+      if (errors.message.includes("password")) {
+        setError("password", {
+          type: "server",
+          message: "Password is incorrect",
+        });
+      }
+    }
+  };
+
   return (
-    <>
-      <div id="sidebar">
-        <h1>Log-in</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
+    <Flex bg="gray.200" align="center" justify="center" h="100vh" w="100vw">
+      <Box bg="white" p={6} rounded="md" w={{ base: "90%", md: "40%" }}>
+        <Heading as="h1" fontSize="4xl" mb={0}>
+          Log in
+        </Heading>
+        <Text fontSize="sm" mb={3}>
+          <chakra.span color="red.600">*</chakra.span> Indicates Required Field
+        </Text>
+
+        {errors?.root?.serverError?.type === 400 && (
+          <Text fontSize="sm" mb={3} color="red.600">
+            {errors?.root.serverError.message}
+          </Text>
+        )}
+
+        <form
+          onSubmit={handleSubmit((stuff) => {
+            console.log("Form submitted", stuff);
+            submitHandler(stuff);
+          })}
+        >
+          <VStack spacing={6} align="flex-start">
+            <InputField
+              name="username"
+              type="text"
+              label="Username"
+              register={register}
+              error={errors.username}
+              id="username"
+              required={true}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div id="detail"></div>
-    </>
+
+            <PasswordField
+              name="password"
+              label="Password"
+              register={register}
+              error={errors.password}
+              id="password"
+              required={true}
+            />
+
+            <Button
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="purple"
+              width="full"
+            >
+              Login
+            </Button>
+          </VStack>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
